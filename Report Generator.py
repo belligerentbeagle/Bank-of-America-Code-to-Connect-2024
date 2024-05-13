c9557d4cfe27a1de7ed8ab5f4fec59117982d2a9
tracy610 <tracytrills6@gmail.com>
2024-05-13 14:06:42 +0800

    Create Report Generator.py

---
+++ Report Generator.py
@@ -0,0 +1,26 @@
+#exchange report
+import pandas as pd
+import numpy as np
+
+array=[1,2,3,4]
+
+def array_to_csv_with_headers(array,column_names,filename):
+
+    array =
+    column_names= ['OrderID','RejectionReason'] #header
+    dataframe = pd.DataFrame(array, column_names) #creating dataframe using array
+    dataframe.to_csv("Exchange_report.csv", index=False) #dataframe to csv
+
+    #client report
+    array =
+    column_names= ['ClientID','InstrumentalID','NetPosition'] #header
+    dataframe = pd.DataFrame(array, column_names) #creating dataframe using array
+    dataframe.to_csv("Client_report.csv",index=False) #dataframe to csv
+
+    #instrumental report
+    array =
+    column_names= ['InstrumentalID','OpenPrice','ClosePrice','TotalVolume','VWAP','DayHigh','DayLow'] #header
+    dataframe = pd.DataFrame(array, column_names) #creating dataframe using array
+    dataframe.to_csv("Instrumental_report.csv",index=False) #dataframe to csv
+
+
